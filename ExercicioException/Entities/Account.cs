06ec61b1d5a5c8c0618344e69fb2cc1e035a0cf6
Ronan Avila <avila.ronan@gmail.com>
2020-02-03 12:43:48 -0300

    Exercicio Exception Resolvido

---
+++ ExercicioException/Entities/Account.cs
@@ -0,0 +1,37 @@
+﻿using ExercicioException.Entities.Exception;
+
+namespace ExercicioException.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; private set; }
+        public double WithDrawLimit { get; private set; }
+
+        public Account(int number, string holder, double balance, double withDrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+        }
+
+        public void Deposit(double ammount)
+        {
+            Balance += ammount;
+        }
+
+        public void WithDraw(double ammount)
+        {
+            if (WithDrawLimit < ammount)
+                throw new DomainException("Withdraw error: The amount exceeds withdraw limit");
+            if (Balance < ammount)
+                throw new DomainException("Withdraw error: Not enough balance");
+
+            Balance -= ammount;
+        }
+
+    }
+
+}
---
+++ ExercicioException/Entities/Exception/DomainException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioException.Entities.Exception
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
